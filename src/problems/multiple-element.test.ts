6d0036b406fed29e23313ab6995e29e6e44f4452
Bayu Cakra <bayu.cakra@fabelio.com>
2020-10-18 21:58:41 +0700

    Add unit test for multipled element

---
+++ src/problems/multiple-element.test.ts
@@ -0,0 +1,13 @@
+import { multipleElement } from './multiple-element';
+
+describe('multiple-element', () => {
+  test('should return correct multipled element', () => {
+    const input = [4, 10, 50, 100];
+
+    const result = multipleElement(input);
+
+    expect(result).toEqual(
+      expect.arrayContaining([16, 100, 2500, 10000])
+    );
+  });
+});
